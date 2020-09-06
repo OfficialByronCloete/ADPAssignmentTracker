35f98f602735ff46db2dc1cc619ef6cfe4e2419a
ChClaude <christ.tchambila@gmail.com>
2020-09-06 21:36:28 +0200

    assignment and vote services added

--- src/main/java/org/assignmentTracker/service/assignment/IAssignmentService.java
+++ src/main/java/org/assignmentTracker/service/assignment/IAssignmentService.java
@@ -1,11 +1,11 @@
 package org.assignmentTracker.service.user;
 
-import org.assignmentTracker.entity.User;
+import org.assignmentTracker.entity.Assignment;
 import org.assignmentTracker.service.IService;
 
 import java.util.Set;
 
-public interface IUserService extends IService<User, Integer> {
-    Set<User> getAll();
+public interface IAssignmentService extends IService<Assigment, Integer> {
+    Set<Assignment> getAll();
     Set<User> getAllSurnamesThatStartWithC();
 }
