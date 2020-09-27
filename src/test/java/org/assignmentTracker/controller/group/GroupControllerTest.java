a6086b870b4d57096653bf9de1d70092ccd6e654
Sinovuyo28 <61121241+Sinovuyo28@users.noreply.github.com>
2020-09-27 14:23:57 +0200

    made corrections on GroupController test class

--- src/test/java/org/assignmentTracker/controller/group/GroupControllerTest.java
+++ src/test/java/org/assignmentTracker/controller/group/GroupControllerTest.java
@@ -34,13 +34,13 @@ public class GroupControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
     private String baseURL = "http://localhost:8080/group";
-    Group group = new Group();
+    Group group = GroupFactory.createGroup( "Group 13","Mr WC Rothman","Project 3");
 
 
     @Test
     public void create() {
 
-        Group group = GroupFactory.createGroup( "Group 13","Mr WC Rothman","Project 3");
+        
         String url = baseURL + "create";
         ResponseEntity <Group> response = restTemplate.postForEntity(url,group, Group.class);
         assertNotNull(response);
@@ -62,7 +62,7 @@ public class GroupControllerTest {
 
     @Test
     public void b_read(){
-
+         Group group = new Group();
         String url = baseURL + "read"+ group.getId();
         System.out.println("URL"+url);
         ResponseEntity <Group> responseEntity = restTemplate.getForEntity(url,Group.class);
@@ -73,6 +73,8 @@ public class GroupControllerTest {
 
     @Test
     public void c_update(){
+        
+        Group group = new Group();
         Group update = new Group.Builder().copy(group).setName("Group thirteen").build();
         String url = baseURL + "update";
         System.out.println("URL:"+ url);
